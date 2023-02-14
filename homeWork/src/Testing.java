9d17d441fe8f21745fb6ddd61467a6ca75a6af86
MrZipka <Tornado-77@yandex.ru>
2023-02-15 01:47:40 +0300

    HomeWorkBranch

---
+++ homeWork/src/Testing.java
@@ -0,0 +1,17 @@
+import javax.swing.text.DefaultStyledDocument;
+import java.util.Scanner;
+
+public class Testing {
+public static void main (String[] args){
+    Scanner sc = new Scanner(System.in);
+    int answer = sc.nextInt();
+
+    if (answer > 0) System.out.println("Неверно");
+    else System.out.println("Верно");
+
+
+    //System.out.println(answer);
+
+}
+}
+
