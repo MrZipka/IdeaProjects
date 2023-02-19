6deb6077650f3d2ea31ef8a5723a8f6ba8f099c5
MrZipka <Tornado-77@yandex.ru>
2023-02-20 00:42:49 +0300

    HomeWork3

---
+++ homeWork/src/TryToCoding.java
@@ -0,0 +1,7 @@
+public class TryToCoding {
+    public static void main(String[] args) {
+        for (int i = 0; i < Questions.questions.length; i++) {
+            System.out.println(i);
+        }
+    }
+}
