a26f64482abadb2ed921266542c852df98e76601
Admin <tornado-77@yandex.ru>
2023-06-07 20:29:41 +0300

    Исправлен тест на ничью

--- homeWork/src/HomeWork8/Tests/GameWinnerConsolePrinter4.java
+++ homeWork/src/HomeWork8/Tests/GameWinnerConsolePrinter4.java
@@ -2,10 +2,10 @@ public class GameWinnerConsolePrinter4 implements GameWinnerPrinter {
     String scenario = "Проверка при ничьей";
     @Override
     public void printWinner(Player winner)  {
-        if (winner.getName().equals("Вася"))
+        if (winner.getName().equals("Ничья"))
         System.out.printf("\"%s\" passed %n", scenario);
         else
-        System.err.printf("\"%s\" faild. При ничьей программа должна определять победителем игрока № 1 %n", scenario);
+        System.err.printf("\"%s\" faild. При ничьей программа должна выдавать ничью %n", scenario);
     }
 
 }
